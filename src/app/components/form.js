98ec1062ff6748fda235f5f5bac99f036bfbf751
FrantiskaAli <tiskarechk@gmail.com>
2023-06-04 22:51:07 +0100

    I attempeted to link first step with the form and failed, however for now i have some validation on the first step"

--- src/app/components/form.js
+++ src/app/components/form.js
@@ -7,30 +7,30 @@ import Thanks from './thanks';
 import { useState } from 'react';
 
 export default function Form() {
-const [formInfo, setInfo] = useState({
-    step: 1,
-    name:'',
-    email:'',
-    tel:'',
-    plan:'',
-    period: '',
-    onlineServices: false,
-    largerStorage:false,
-    customProfile: false,
-})
-const prevStep = () => {
-    setInfo({step: step - 1} );
-  }
-  const nextStep = () => {
-    setInfo({ step: step + 1} );
-  }
-const handleChange = (e) => {
-    setInfo({ [e.target.id]: e.target.value });
-  }
+    const [formInfo, setInfo] = useState({
+        step: 1,
+        name: '',
+        email: '',
+        tel: '',
+        plan: '',
+        period: '',
+        onlineServices: false,
+        largerStorage: false,
+        customProfile: false,
+    })
+    const prevStep = () => {
+        setInfo({step: formInfo.step - 1});
+    }
+    const nextStep = () => {
+        setInfo({step: formInfo.step + 1});
+    };
+    const handleChange = (e) => {
+        setInfo({ [e.target.id]: e.target.value });
+    }
     switch (formInfo.step) {
         case 1:
             return (
-                <StepOne />
+                <StepOne change={handleChange} next={nextStep} />
             )
         case 2:
             return (
--- src/app/components/step-1.js
+++ src/app/components/step-1.js
@@ -1,22 +1,63 @@
-
+'use client'
 
 export default function StepOne({change, next}) {
+function handleClick(){
+  const email = document.getElementById('email');
+const name =document.getElementById('name');
+const number =document.getElementById('tel');
+let moveOn = 0
+const personalInfo = [email, name, number];
+//this for loop inside click on function makes sure user is alerted if input is empty
+for (let i=0; i< personalInfo.length; i++){
+  const input = personalInfo[i]
+  switch(input){
+    case 'name':
+      if (!name.value){
+        console.log('please enter your name')
+      } else moveOn++;
+      break;
+      case 'email':
+        const emailValue = email.value
+        let re = new RegExp();
+        re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(emailValue)) {
+          console.log('please enter ur email')} else moveOn++;
+      break;
+      case 'number':
+        const tel = number.value;
+        let reg = new RegExp();
+        reg =/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+        if(!reg.test(tel)){
+          console.log('please enter valid phone number')
+        }else moveOn++;
+      break;
+      default:
+  }
+
+}
+
+if( moveOn == 3){
+  next()
+}
+}
+
+
   return (
     <>
     <h1>Personal info</h1>
     
     <h2>Please provide your name, email address, and phone number.</h2>
     
-    <section><label for='name'>Name</label></section>
+    <section><label htmlFor='name'>Name</label></section>
     <input type='text' id='name' name='name' placeholder="e.g. Stephen King" onChange={change}/>
 
-    <section><label for='email'>Email address</label></section>
+    <section><label htmlFor='email'>Email address</label></section>
     <input type='text' id='email' name='email' placeholder="  e.g. stephenking@lorem.com" onChange={change} />
 
-    <section><label for='tel'>Phone Number</label></section>
+    <section><label htmlFor='tel'>Phone Number</label></section>
     <input type='text' id='tel' name='tel' placeholder="e.g. +1 234 567 890" onChange={change}/>
   
-    <button onClick={next} >Next step</button>
+    <button onClick={()=>handleClick()} >Next step</button>
     
     </>
   )
